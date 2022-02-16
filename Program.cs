0dab90089ebfa27577c720e94471294a75b64952
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2022-02-16 10:50:12 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Files and Streams Debug 1
+ * Pawelski
+ * 2/16/2022
+ * This program does not work! Find and fix any bugs
+ * in the code so that the program works as described
+ * on the activity sheet. In addition, describe the 
+ * bugs and how you fixed them on the activity sheet.
+ */
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesAndStreamsDebug1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Please add the correct file path! Do not include this in your debugging.
+            const string FILE_PATH = @"";
+            FileStream file = new FileStream(FILE_PATH, FileMode.Open, FileAccess.Read);
+            StreamWriter reader = new StreamWriter(file);
+            while(!reader.EndOfStream)
+            {
+                Console.WriteLine(reader.ReadLine());
+            }
+            reader.Close();
+            file.Close();
+        }
+    }
+}
